a47c59543820edcfdeceb827deb96ddbe480164f
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2023-03-21 16:31:18 -0300

    In statement

---
+++ 09_in_statement/index.py
@@ -0,0 +1,13 @@
+beatles = ["John", "Paul", "Ringo", "George"]
+
+print("Paul" in beatles)
+print("Roger" in beatles)
+
+book = "Moby Dick"
+
+print("oby" in book)
+
+movies = {"The Matrix", "2001", "The Shining"}
+user_movie = input("Enter a movie you've watched recently:\n>> ")
+
+print(user_movie in movies)
