2721a734ea6e3867866e19eeb20197d83db4d41a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2023-03-21 16:00:26 -0300

    If statement

---
+++ 08_if_statements/index.py
@@ -0,0 +1,17 @@
+day_of_week = input("What day of the week is it today?\n>> ").lower()
+
+# print(day_of_week == "Monday")
+
+if day_of_week == "monday":
+  print("Have a great start to your week!")
+elif day_of_week == "friday":
+  print("Hava a nice weekend!")
+else:
+  print("Full speed ahead!")
+
+
+# if day_of_week != "Monday":
+#   print("Full speed ahead!")
+
+print("This always runs")
+
