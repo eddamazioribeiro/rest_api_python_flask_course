5fd8105a8378ae81540cf0b8538e6039d7f1c512
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2023-03-21 15:15:30 -0300

    Booleans

---
+++ 07_booleans/index.py
@@ -0,0 +1,10 @@
+print(5==5)
+print(5 > 5)
+
+friends = ["John", "Paul"]
+abroad = ["John", "Paul"]
+
+# is check if the elements are the same, same variable in memory
+print(friends == abroad)
+print(friends is abroad)
+print(friends is friends)
