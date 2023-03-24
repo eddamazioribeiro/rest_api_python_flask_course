e00df9e6fb19add8b0edc6af36cb13992715bdbc
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2023-03-24 17:51:36 -0300

    Import in Python

---
+++ 29_imports/index.py
@@ -0,0 +1,6 @@
+from my_module import divide
+import sys
+
+print(divide(10, 5))
+print(sys.path)
+print(sys.modules)
---
+++ 29_imports/my_module.py
@@ -0,0 +1,6 @@
+def divide(dividend, divisor) -> float:
+  if divisor == 0:
+    return
+  return dividend / divisor
+
+print("my_module.py: ", __name__)
